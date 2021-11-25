c62eb6f12ccf6bbbb17ca7affc1e636605df42e5
yoga <80277262+yogaprasetya22@users.noreply.github.com>
2021-11-25 10:19:15 +0700

    Update con.py
    asd

--- con.py
+++ con.py
@@ -2,7 +2,7 @@ from cba import c
 
 
 def panggil():
-    strfile = input(c.ly+"Nama File: ")
+    strfile = input("Nama File: ")
     strfile = strfile + ".txt"
     return strfile
 
@@ -11,12 +11,14 @@ def soal():
     file = panggil()
     file = open(file, "a")
     nom = input('convert: ')
-    nom = nom.replace(',', '\n')
+    nom = nom.replace('-', '')
+    nom = nom.replace(' ', '')
+    nom = nom.replace(',', '')
+    nom = nom.replace('+62', '\n')
     strmasuk = nom
     file.write(strmasuk)
     print(strmasuk)
     file.close()
-    print(file)
 
 
 soal()
